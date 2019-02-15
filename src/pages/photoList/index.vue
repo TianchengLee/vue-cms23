778fa9dba51acf616d993c4ed4561afa0460ee83
TianchengLee <ltc6634284@gmail.com>
2019-02-15 16:01:47 +0800

    图片列表完成

--- src/pages/photoList/index.vue
+++ src/pages/photoList/index.vue
@@ -7,20 +7,23 @@
       >
         <div class="mui-scroll">
           <a
-            class="mui-control-item mui-active"
-            href="#item1mobile"
-            data-wid="tab-top-subpage-1.html"
-          >推荐</a>
-          <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">热点</a>
-          <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">北京</a>
-          <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">社会</a>
-          <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">娱乐</a>
-          <!--<a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
-							科技
-          </a>-->
+            :class="['mui-control-item', { 'mui-active' : item.id == 0 } ]"
+            v-for="(item) in categories"
+            :key="item.id"
+          >{{item.title}}</a>
         </div>
       </div>
     </div>
+    <!-- 图片列表区域 -->
+    <ul class="photo-list">
+      <router-link v-for="item in photoList" :key="item.id" :to="'/home/photoinfo/' + item.id" tag="li">
+        <img v-lazy="item.img_url">
+        <div class="info">
+          <h1 class="info-title">{{ item.title }}</h1>
+          <div class="info-body">{{ item.zhaiyao }}</div>
+        </div>
+      </router-link>
+    </ul>
   </div>
 </template>
 
@@ -37,14 +40,84 @@ import mui from "@/libs/mui/js/mui.min.js";
 // 解决方案: 在 .babelrc 配置中添加js文件的忽略
 
 // bug4: 解决完以上Bug之后, tabBar无法切换了
-// 解决方案: 将mui-tab-item类名换一个即可 
+// 解决方案: 将mui-tab-item类名换一个即可
 export default {
+  created() {
+    this.getCategories();
+    this.getPhotoList(0);
+  },
   mounted() {
     // bug3: 第一次进入时无法滚动
     // 初始化操作必须要放在mounted钩子中, 让DOM元素加载完毕后才可以初始化
     mui(".mui-scroll-wrapper").scroll({
       deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     });
+  },
+  data() {
+    return {
+      categories: [],
+      photoList: []
+    };
+  },
+  methods: {
+    getCategories() {
+      this.$http.get("getimgcategory").then(result => {
+        this.categories = result.body.message;
+        this.categories.unshift({
+          id: 0,
+          title: "全部"
+        });
+      });
+    },
+    getPhotoList(cateId) {
+      this.$http.get("getimages/" + cateId).then(result => {
+        this.photoList = result.body.message;
+      });
+    }
   }
 };
 </script>
+
+<style lang="less">
+* {
+  touch-action: pan-y;
+}
+
+.photo-list {
+  list-style: none;
+  margin: 0;
+  padding: 10px;
+  padding-bottom: 0;
+  li {
+    background-color: #ccc;
+    text-align: center;
+    margin-bottom: 10px;
+    box-shadow: 0 0 9px #999;
+    position: relative;
+    img {
+      width: 100%;
+      vertical-align: middle;
+    }
+    img[lazy="loading"] {
+      width: 40px;
+      height: 300px;
+      margin: auto;
+    }
+
+    .info {
+      color: white;
+      text-align: left;
+      position: absolute;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.4);
+      max-height: 84px;
+      .info-title {
+        font-size: 14px;
+      }
+      .info-body {
+        font-size: 13px;
+      }
+    }
+  }
+}
+</style>
